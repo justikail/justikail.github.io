fd769be827b50bbc26fe5ef58bf98d49c9aaa160
Haikal Siregar <mininginformation1337@gmail.com>
2024-01-20 10:50:25 +0700

    Create dumb.js

---
+++ dumb.js
@@ -0,0 +1,16 @@
+<div style="display: none">
+  <ul>
+    <li>https://bsk.kemenkumham.go.id/blog/mania/</li>
+    <li>https://bsk.kemenkumham.go.id/media/pgsoft/</li>
+    <li>https://bsk.kemenkumham.go.id/storage/idn/</li>
+    <li>https://bsk.kemenkumham.go.id/js/wmcasino/</li>
+    <li>https://bsk.kemenkumham.go.id/assets/ws168/</li>
+    <li>https://bsk.kemenkumham.go.id/css/digmaan/</li>
+    <li>https://bsk.kemenkumham.go.id/products/honeybet303/</li>
+    <li>https://bsk.kemenkumham.go.id/po-content/products/sv388/</li>
+    <li>https://e-litbang.balitbangham.go.id/captcha/dombatoto/</li>
+    <li>https://e-litbang.balitbangham.go.id/captcha/toto-4d/</li>
+    <li>https://pmb.stimsukmamedan.ac.id/blog/toto4d/</li>
+    <li>https://ekebijakan-bsk.kemenkumham.go.id/asset/totomacau/</li>
+  </ul>
+</div>
