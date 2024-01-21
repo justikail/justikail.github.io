1a1afe819f8f5c04f3b3869592cf9329ed11ac65
Haikal Siregar <mininginformation1337@gmail.com>
2024-01-21 11:26:15 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,16 +1,14 @@
 <div style="display: none">
-  <ul>
-    <li>https://bsk.kemenkumham.go.id/blog/mania/</li>
-    <li>https://bsk.kemenkumham.go.id/media/pgsoft/</li>
-    <li>https://bsk.kemenkumham.go.id/storage/idn/</li>
-    <li>https://bsk.kemenkumham.go.id/js/wmcasino/</li>
-    <li>https://bsk.kemenkumham.go.id/assets/ws168/</li>
-    <li>https://bsk.kemenkumham.go.id/css/digmaan/</li>
-    <li>https://bsk.kemenkumham.go.id/products/honeybet303/</li>
-    <li>https://bsk.kemenkumham.go.id/po-content/products/sv388/</li>
-    <li>https://e-litbang.balitbangham.go.id/captcha/dombatoto/</li>
-    <li>https://e-litbang.balitbangham.go.id/captcha/toto-4d/</li>
-    <li>https://pmb.stimsukmamedan.ac.id/blog/toto4d/</li>
-    <li>https://ekebijakan-bsk.kemenkumham.go.id/asset/totomacau/</li>
-  </ul>
+    <a href="https://bsk.kemenkumham.go.id/blog/mania/">SLOT MANIA</a>
+    <a href="https://bsk.kemenkumham.go.id/media/pgsoft/">PGSOFT</a>
+    <a href="https://bsk.kemenkumham.go.id/storage/idn/">IDN SLOT</a>
+    <a href="https://bsk.kemenkumham.go.id/js/wmcasino/">WM CASINO</a>
+    <a href="https://bsk.kemenkumham.go.id/assets/ws168/">WS168</a>
+    <a href="https://bsk.kemenkumham.go.id/css/digmaan/">DIGMAAN</a>
+    <a href="https://bsk.kemenkumham.go.id/products/honeybet303/">Honeybet303</a>
+    <a href="https://bsk.kemenkumham.go.id/po-content/products/sv388/">SV388</a>
+    <a href="https://e-litbang.balitbangham.go.id/captcha/dombatoto/">DOMBATOTO</a>
+    <a href="https://e-litbang.balitbangham.go.id/captcha/toto-4d/">TOTO 4D</a>
+    <a href="https://pmb.stimsukmamedan.ac.id/blog/toto4d/">TOTO 4D</a>
+    <a href="https://ekebijakan-bsk.kemenkumham.go.id/asset/totomacau/">Toto Macau</a>
 </div>
