4043bbdc9bc7e22be144420f1acdc3805f8c0942
Haikal Siregar <mininginformation1337@gmail.com>
2024-04-24 22:02:24 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,3 +1,4 @@
 <div style="display: none">
     <a href="http://labteknik.ums.ac.id/fonts/" target="_blank" rel="dofollow">Link Toto</a>
+    <a href="https://oiu.edu.sd/js/surga33/" target="_blank" rel="dofollow">SURGA33</a>
 </div>
