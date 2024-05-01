e2342b054806a7446a1fd728fc5486ec1c169182
Haikal Siregar <mininginformation1337@gmail.com>
2024-05-01 20:51:57 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -6,4 +6,9 @@
     <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=PRESIDENSLOT" target="_blank" rel="dofollow">PRESIDENSLOT</a>
     <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=PGSLOT" target="_blank" rel="dofollow">PGSLOT</a>
     <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=QQSLOT" target="_blank" rel="dofollow">QQSLOT</a>
+    <a href="https://intrase.edu.vn/app/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
+    <a href="https://intrase.edu.vn/app/?products=PANDORA88" target="_blank" rel="dofollow">PANDORA88</a>
+    <a href="https://intrase.edu.vn/app/?products=ZUES138" target="_blank" rel="dofollow">ZUES138</a>
+    <a href="https://intrase.edu.vn/app/?products=KOITOTO" target="_blank" rel="dofollow">KOITOTO</a>
+    <a href="https://intrase.edu.vn/app/?products=BABABOLA" target="_blank" rel="dofollow">BABABOLA</a>
 </div>
