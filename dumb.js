74c0c004180778508050b821d30fbafcbf3c89fe
Haikal Siregar <mininginformation1337@gmail.com>
2024-05-01 20:47:15 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,4 +1,9 @@
 <div style="display: none">
     <a href="http://labteknik.ums.ac.id/fonts/" target="_blank" rel="dofollow">Link Toto</a>
     <a href="https://oiu.edu.sd/js/surga33/" target="_blank" rel="dofollow">SURGA33</a>
+    <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=slot5000" target="_blank" rel="dofollow">slot5000</a>
+    <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=MEGASLOT" target="_blank" rel="dofollow">MEGASLOT</a>
+    <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=PRESIDENSLOT" target="_blank" rel="dofollow">PRESIDENSLOT</a>
+    <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=PGSLOT" target="_blank" rel="dofollow">PGSLOT</a>
+    <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=QQSLOT" target="_blank" rel="dofollow">QQSLOT</a>
 </div>
