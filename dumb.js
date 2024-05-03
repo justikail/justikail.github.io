254250fb7d7fb48471e454fb16665cc18cc20e37
Haikal Siregar <mininginformation1337@gmail.com>
2024-05-03 20:36:16 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -6,11 +6,6 @@
     <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=PRESIDENSLOT" target="_blank" rel="dofollow">PRESIDENSLOT</a>
     <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=PGSLOT" target="_blank" rel="dofollow">PGSLOT</a>
     <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=QQSLOT" target="_blank" rel="dofollow">QQSLOT</a>
-    <a href="https://intrase.edu.vn/app/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
-    <a href="https://intrase.edu.vn/app/?products=PANDORA88" target="_blank" rel="dofollow">PANDORA88</a>
-    <a href="https://intrase.edu.vn/app/?products=ZUES138" target="_blank" rel="dofollow">ZUES138</a>
-    <a href="https://intrase.edu.vn/app/?products=KOITOTO" target="_blank" rel="dofollow">KOITOTO</a>
-    <a href="https://intrase.edu.vn/app/?products=BABABOLA" target="_blank" rel="dofollow">BABABOLA</a>
     <a href="www.milkwood.nt.edu.au/assets/slot-raffi-ahmad-77/" target="_blank" rel="dofollow">slot raffi ahmad 77</a>
     <a href="https://www.milkwood.nt.edu.au/js/?ribaksude=Olympus138" target="_blank" rel="dofollow">Olympus138</a>
     <a href="https://www.milkwood.nt.edu.au/js/?ribaksude=Starslot777" target="_blank" rel="dofollow">Starslot777</a>
@@ -21,4 +16,9 @@
     <a href="https://dkxt.phenikaa-uni.edu.vn/js/?products=dewaslot" target="_blank" rel="dofollow">dewaslot</a>
     <a href="https://dkxt.phenikaa-uni.edu.vn/js/?products=pashoki" target="_blank" rel="dofollow">pashoki</a>
     <a href="https://dkxt.phenikaa-uni.edu.vn/js/?products=slot838" target="_blank" rel="dofollow">slot838</a>
+    <a href="https://oiu.edu.sd/css/?products=SIMBA4D" target="_blank" rel="dofollow">SIMBA4D</a>
+    <a href="https://oiu.edu.sd/css/?products=PLANET77" target="_blank" rel="dofollow">PLANET77</a>
+    <a href="https://oiu.edu.sd/css/?products=VEGASHOKI" target="_blank" rel="dofollow">VEGASHOKI</a>
+    <a href="https://oiu.edu.sd/css/?products=UNGGUL88" target="_blank" rel="dofollow">UNGGUL88</a>
+    <a href="https://oiu.edu.sd/css/?products=MENTOL4D" target="_blank" rel="dofollow">MENTOL4D</a>
 </div>
