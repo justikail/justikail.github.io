558bd6fb8c03ecf267f58b0a650faeb58f22a226
Haikal Siregar <mininginformation1337@gmail.com>
2024-05-13 17:25:28 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,12 +1,12 @@
 <div style="display: none">
-    <a href="http://labteknik.ums.ac.id/fonts/" target="_blank" rel="dofollow">Link Toto</a>
+    <!-- <a href="http://labteknik.ums.ac.id/fonts/" target="_blank" rel="dofollow">Link Toto</a> -->
     <a href="https://oiu.edu.sd/js/surga33/" target="_blank" rel="dofollow">SURGA33</a>
-    <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=slot5000" target="_blank" rel="dofollow">slot5000</a>
+    <!-- <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=slot5000" target="_blank" rel="dofollow">slot5000</a>
     <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=MEGASLOT" target="_blank" rel="dofollow">MEGASLOT</a>
     <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=PRESIDENSLOT" target="_blank" rel="dofollow">PRESIDENSLOT</a>
     <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=PGSLOT" target="_blank" rel="dofollow">PGSLOT</a>
-    <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=QQSLOT" target="_blank" rel="dofollow">QQSLOT</a>
-    <a href="www.milkwood.nt.edu.au/assets/slot-raffi-ahmad-77/" target="_blank" rel="dofollow">slot raffi ahmad 77</a>
+    <a href="https://web.cgehs.tyc.edu.tw:10000/js/?products=QQSLOT" target="_blank" rel="dofollow">QQSLOT</a> -->
+    <a href="http://www.milkwood.nt.edu.au/assets/slot-raffi-ahmad-77/" target="_blank" rel="dofollow">slot raffi ahmad 77</a>
     <a href="https://www.milkwood.nt.edu.au/js/?ribaksude=Olympus138" target="_blank" rel="dofollow">Olympus138</a>
     <a href="https://www.milkwood.nt.edu.au/js/?ribaksude=Starslot777" target="_blank" rel="dofollow">Starslot777</a>
     <a href="https://www.milkwood.nt.edu.au/js/?ribaksude=Medantoto" target="_blank" rel="dofollow">Medantoto</a>
@@ -21,17 +21,24 @@
     <a href="https://oiu.edu.sd/css/?products=VEGASHOKI" target="_blank" rel="dofollow">VEGASHOKI</a>
     <a href="https://oiu.edu.sd/css/?products=UNGGUL88" target="_blank" rel="dofollow">UNGGUL88</a>
     <a href="https://oiu.edu.sd/css/?products=MENTOL4D" target="_blank" rel="dofollow">MENTOL4D</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/img/sv388" target="_blank" rel="dofollow">sv388</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/assets/digmaan" target="_blank" rel="dofollow">digmaan</a>
+    <a href="http://educacion.saludcoahuila.gob.mx/img/sv388/" target="_blank" rel="dofollow">sv388</a>
+    <a href="http://educacion.saludcoahuila.gob.mx/assets/digmaan/" target="_blank" rel="dofollow">digmaan</a>
     <a href="http://educacion.saludcoahuila.gob.mx/products/ws168/" target="_blank" rel="dofollow">ws168</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/css/slot-petir" target="_blank" rel="dofollow">slot petir</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/uploads/slot-receh" target="_blank" rel="dofollow">slot receh</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/images/pgsoft" target="_blank" rel="dofollow">pgsoft</a>
-    <a href="http://sos.agri.kps.ku.ac.th/assets/fin4d" target="_blank" rel="dofollow">fin4d</a>
-    <a href="http://sos.agri.kps.ku.ac.th/images/ajaib88" target="_blank" rel="dofollow">ajaib88</a>
+    <a href="http://educacion.saludcoahuila.gob.mx/css/slot-petir/" target="_blank" rel="dofollow">slot petir</a>
+    <a href="http://educacion.saludcoahuila.gob.mx/uploads/slot-receh/" target="_blank" rel="dofollow">slot receh</a>
+    <a href="http://educacion.saludcoahuila.gob.mx/images/pgsoft/" target="_blank" rel="dofollow">pgsoft</a>
+    <!-- <a href="http://sos.agri.kps.ku.ac.th/assets/fin4d" target="_blank" rel="dofollow">fin4d</a> -->
+    <a href="http://sos.agri.kps.ku.ac.th/images/ajaib88/" target="_blank" rel="dofollow">ajaib88</a>
     <a href="https://bsk.kemenkumham.go.id/files/pos4d/" target="_blank" rel="dofollow">pos4d</a>
     <a href="https://bsk.kemenkumham.go.id/captcha/fin4d/" target="_blank" rel="dofollow">fin4d</a>
     <a href="https://bsk.kemenkumham.go.id/admin/inatogel/" target="_blank" rel="dofollow">inatogel</a>
     <a href="https://bsk.kemenkumham.go.id/file/bimabet/" target="_blank" rel="dofollow">bimabet</a>
     <a href="https://bsk.kemenkumham.go.id/asset/area188/" target="_blank" rel="dofollow">area188</a>
+    <a href="https://e-journal.iaingorontalo.ac.id/files/fin4d/" target="_blank" rel="dofollow">fin4d</a>
+    <a href="https://e-journal.iaingorontalo.ac.id/img/bimabet/" target="_blank" rel="dofollow">bimabet</a>
+    <a href="https://e-journal.iaingorontalo.ac.id/assets/pos4d/" target="_blank" rel="dofollow">pos4d</a>
+    <a href="http://educacion.saludcoahuila.gob.mx/app/?products=PEDRO4D" target="_blank" rel="dofollow">PEDRO4D</a>
+    <a href="http://educacion.saludcoahuila.gob.mx/app/?products=Ajaib88" target="_blank" rel="dofollow">Ajaib88</a>
+    <a href="http://educacion.saludcoahuila.gob.mx/app/?products=opaltogel" target="_blank" rel="dofollow">opaltogel</a>
+    <a href="http://educacion.saludcoahuila.gob.mx/app/?products=agentoto88" target="_blank" rel="dofollow">agentoto88</a>
 </div>
