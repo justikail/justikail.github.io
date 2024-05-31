4068c342f6f45dc88ec9895c078b5fab4f069fc3
Haikal Siregar <mininginformation1337@gmail.com>
2024-05-31 21:05:01 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,31 +1,14 @@
 <div style="display: none">
-    <a href="http://www.milkwood.nt.edu.au/assets/slot-raffi-ahmad-77/" target="_blank" rel="dofollow">slot raffi ahmad 77</a>
-    <a href="https://www.milkwood.nt.edu.au/js/?ribaksude=Olympus138" target="_blank" rel="dofollow">Olympus138</a>
-    <a href="https://www.milkwood.nt.edu.au/js/?ribaksude=Starslot777" target="_blank" rel="dofollow">Starslot777</a>
-    <a href="https://www.milkwood.nt.edu.au/js/?ribaksude=Medantoto" target="_blank" rel="dofollow">Medantoto</a>
-    <a href="https://www.milkwood.nt.edu.au/js/?ribaksude=Jostoto" target="_blank" rel="dofollow">Jostoto</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/img/sv388/" target="_blank" rel="dofollow">sv388</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/assets/digmaan/" target="_blank" rel="dofollow">digmaan</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/products/ws168/" target="_blank" rel="dofollow">ws168</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/css/slot-petir/" target="_blank" rel="dofollow">slot petir</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/uploads/slot-receh/" target="_blank" rel="dofollow">slot receh</a>
-    <a href="http://educacion.saludcoahuila.gob.mx/images/pgsoft/" target="_blank" rel="dofollow">pgsoft</a>
-    <a href="https://e-journal.iaingorontalo.ac.id/files/fin4d/" target="_blank" rel="dofollow">fin4d</a>
-    <a href="https://e-journal.iaingorontalo.ac.id/img/bimabet/" target="_blank" rel="dofollow">bimabet</a>
-    <a href="https://e-journal.iaingorontalo.ac.id/assets/pos4d/" target="_blank" rel="dofollow">pos4d</a>
     <a href="http://educacion.saludcoahuila.gob.mx/app/?products=PEDRO4D" target="_blank" rel="dofollow">PEDRO4D</a>
     <a href="http://educacion.saludcoahuila.gob.mx/app/?products=Ajaib88" target="_blank" rel="dofollow">Ajaib88</a>
     <a href="http://educacion.saludcoahuila.gob.mx/app/?products=opaltogel" target="_blank" rel="dofollow">opaltogel</a>
     <a href="http://educacion.saludcoahuila.gob.mx/app/?products=agentoto88" target="_blank" rel="dofollow">agentoto88</a>
-    <a href="https://cste.sut.ac.th/js/?products=ajaib88" target="_blank" rel="dofollow">ajaib88</a>
-    <a href="https://cste.sut.ac.th/js/?products=Megaslot88" target="_blank" rel="dofollow">Megaslot88</a>
     <a href="https://www.smkpgri1surabaya.sch.id/js/?products=MAWARTOTO" target="_blank" rel="dofollow">MAWARTOTO</a>
     <a href="https://aceiot.ur.ac.rw/images/?products=MAMIBET" target="_blank" rel="dofollow">MAMIBET</a>
     <a href="https://smanda-bdl.sch.id/assets/?products=API5000" target="_blank" rel="dofollow">API5000</a>
     <a href="https://fisip.unsa.ac.id/assets/?products=SURGA11" target="_blank" rel="dofollow">SURGA11</a>
     <a href="http://gizi.fkm.unsri.ac.id/js/?products=SENIOR4D" target="_blank" rel="dofollow">SENIOR4D</a>
     <a href="https://toyotadealer.co.id/wp-content/?products=SURGA88" target="_blank" rel="dofollow">SURGA88</a>
-    <a href="https://man1mojokerto.sch.id/assets/?products=SENIOR4D">SENIOR4D</a>
     <a href="https://mutiaragemilang.id/js/?products=SENIOR4D" target="_blank" rel="dofollow">SENIOR4D</a>
     <a href="https://smanegeri1seputihagung.sch.id/?products=SENIOR4D" target="_blank" rel="dofollow">SENIOR4D</a>
     <a href="https://www.sman1tegaldlimo.sch.id/css/?products=SENIOR4D" target="_blank" rel="dofollow">SENIOR4D</a>
