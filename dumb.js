f895f3a2c151026b9a524c5446f41ac9d8d40bc1
Haikal Siregar <mininginformation1337@gmail.com>
2024-07-18 21:39:38 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,43 +1,23 @@
 <div style="display: none">
     <a href="https://heylink.me/sadabet138/" target="_blank" rel="dofollow">Sadabet138</a>
-    <a href="https://smkn1ramanutara.sch.id/assets/?products=SENIOR4D" target="_blank" rel="dofollow">SENIOR4D</a>
-    <a href="https://smpn8purwokerto.sch.id/" target="_blank" rel="dofollow">https://smpn8purwokerto.sch.id/</a>
-    <a href="https://solahartcenter.id/" target="_blank" rel="dofollow">https://solahartcenter.id/</a>
-    <a href="https://unbp.ac.id/images/ac/sv388/" target="_blank" rel="dofollow">sv388</a>
-    <a href="https://unbp.ac.id/products/co/digmaan/" target="_blank" rel="dofollow">digmaan</a>
-    <a href="https://unbp.ac.id/vendor/sch/slotpetir/" target="_blank" rel="dofollow">slot petir</a>
     <a href="https://skpiso.id/css/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
-    <a href="https://penjaminanmutu.stikeshangtuah-sby.ac.id/js/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
-    <a href="https://englishversity.id/apps/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
-    <a href="https://kopertais15.or.id/assets/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
-    <a href="https://proketra.man1mojokerto.sch.id/vendor/inatogel/" target="_blank" rel="dofollow">inatogel</a>
-    <a href="https://proketra.man1mojokerto.sch.id/vendor/area188/" target="_blank" rel="dofollow">area188</a>
-    <a href="https://proketra.man1mojokerto.sch.id/vendor/ziatogel/" target="_blank" rel="dofollow">ziatogel</a>
-    <a href="https://proketra.man1mojokerto.sch.id/vendor/kepritogel/" target="_blank" rel="dofollow">kepritogel</a>
-    <a href="https://proketra.man1mojokerto.sch.id/vendor/fin4d/" target="_blank" rel="dofollow">fin4d</a>
     <a href="https://ppdb.smanegeri1seputihagung.sch.id/images/" target="_blank" rel="dofollow">https://ppdb.smanegeri1seputihagung.sch.id/images/</a>
     <a href="https://spm.unbita.ac.id/js/" target="_blank" rel="dofollow">https://spm.unbita.ac.id/js/</a>
     <a href="https://talentsmart.co.id/js/" target="_blank" rel="dofollow">https://talentsmart.co.id/js/</a>
-    <a href="https://smpn8purwokerto.sch.id/js/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
     <a href="https://smanegeri1seputihmataram.sch.id/js/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
     <a href="https://kelulusan.smanegeri1seputihmataram.sch.id/js/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
     <a href="https://gtp.or.id/js/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
     <a href="https://sava2.talentsmart.co.id/assets/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
     <a href="https://tiraipelajarmandalika.or.id/assets/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
     <a href="https://kostcoliving.id/storage/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
-    <a href="https://boxplus.id/css/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
-    <a href="https://englishversity.id/apps/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
     <a href="https://sdh.or.id/assets/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
     <a href="https://gtp.or.id/folder/org/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
     <a href="https://skpiso.id/assets/img/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
-    <a href="https://englishversity.id/js/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
     <a href="https://mutiaragemilang.id/images/go/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
-    <a href="https://solahartcenter.id/assets/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
     <a href="https://tiraipelajarmandalika.or.id/acc/js/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
     <a href="https://ppdb.smkpgri1surabaya.sch.id/uploads/images/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
     <a href="https://ppdb.smanegeri1seputihagung.sch.id/data/dokumen/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
     <a href="https://ppdb.smkpabhara.sch.id/assets/css/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
-    <a href="https://smpn8purwokerto.sch.id/apps/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
     <a href="https://sdh.or.id/vendor/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
     <a href="https://ppdb.smkpgri1surabaya.sch.id/js/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
     <a href="https://ppdb.smkpabhara.sch.id/images/?products=POS4D" target="_blank" rel="dofollow">POS4D</a>
