d95126980313208227900da4c7c4d61564f97658
Haikal Siregar <mininginformation1337@gmail.com>
2024-08-13 22:19:33 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,34 +1,23 @@
 <div style="display: none">
     <a href="https://heylink.me/sadabet138/" target="_blank" rel="dofollow">Sadabet138</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/sadabet138/" target="_blank" rel="dofollow">sadabet138</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/dewatogel/" target="_blank" rel="dofollow">dewatogel</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/eurotogel/" target="_blank" rel="dofollow">eurotogel</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/gengtoto/" target="_blank" rel="dofollow">gengtoto</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/hometogel/" target="_blank" rel="dofollow">hometogel</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/inatogel/" target="_blank" rel="dofollow">inatogel</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/lunatogel/" target="_blank" rel="dofollow">lunatogel</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/pos4d/" target="_blank" rel="dofollow">pos4d</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/togelon/" target="_blank" rel="dofollow">togelon</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/togelup/" target="_blank" rel="dofollow">togelup</a>
-    <a href="https://smk-triarga2.sch.id/wp-content/uploads/yowestogel/" target="_blank" rel="dofollow">yowestogel</a>
-    <a href="https://perpus.mtsnsatubaubau.sch.id/program/media/sv388/" target="_blank" rel="dofollow">sv388</a>
-    <a href="https://perpus.mtsnsatubaubau.sch.id/media/tech/mahjong-200perak/" target="_blank" rel="dofollow">mahjong 200 perak</a>
-    <a href="https://perpus.mtsnsatubaubau.sch.id/products/css/scatter-hitam/" target="_blank" rel="dofollow">scatter hitam</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/web/products/togelup/" target="_blank" rel="dofollow">togelup</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/web/products/eurotogel/" target="_blank" rel="dofollow">eurotogel</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/web/products/gengtoto/" target="_blank" rel="dofollow">gengtoto</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/web/products/togelon/" target="_blank" rel="dofollow">togelon</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/css/product/ws168/" target="_blank" rel="dofollow">ws168</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/web/products/inatogel/" target="_blank" rel="dofollow">inatogel</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/web/products/dewatogel/" target="_blank" rel="dofollow">dewatogel</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/web/products/yowestogel/" target="_blank" rel="dofollow">yowestogel</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/web/products/hometogel/" target="_blank" rel="dofollow">hometogel</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/web/products/lunatogel/" target="_blank" rel="dofollow">lunatogel</a>
-    <a href="https://www.muhammadiyahcibiuk.or.id/web/products/pos4d/" target="_blank" rel="dofollow">pos4d</a>
-    <a href="https://sigma.dpd.go.id/assets/raja888/" target="_blank" rel="dofollow">raja888</a>
-    <a href="https://sigma.dpd.go.id/img/-/royal777/" target="_blank" rel="dofollow">royal777</a>
-    <a href="https://sigma.dpd.go.id/data/" target="_blank" rel="dofollow">posjitu</a>
-    <a href="https://ppdb.mambalam.sch.id/" target="_blank" rel="dofollow">sadabet138</a>
-    <a href="https://stiti-kp.ac.id/wp-content/uploads/funbet77/" target="_blank" rel="dofollow">funbet77</a>
-    <a href="https://ponxxi.sumutprov.go.id/data/" target="_blank" rel="dofollow">PGSOFT</a>
+    <a href="https://ictcellsupport.just.edu.bd/surga11" target="_blank" rel="dofollow">surga11</a>
+    <a href="https://ictcellsupport.just.edu.bd/surga22" target="_blank" rel="dofollow">surga22</a>
+    <a href="https://ictcellsupport.just.edu.bd/inatogel" target="_blank" rel="dofollow">inatogel</a>
+    <a href="https://ictcellsupport.just.edu.bd/pos4d" target="_blank" rel="dofollow">pos4d</a>
+    <a href="https://ictcellsupport.just.edu.bd/togelon" target="_blank" rel="dofollow">togelon</a>
+    <a href="https://ictcellsupport.just.edu.bd/togelup" target="_blank" rel="dofollow">togelup</a>
+    <a href="https://ictcellsupport.just.edu.bd/dewatogel" target="_blank" rel="dofollow">dewatogel</a>
+    <a href="https://somoyerkhbor.com/surga11" target="_blank" rel="dofollow">surga11</a>
+    <a href="https://somoyerkhbor.com/surga88" target="_blank" rel="dofollow">surga88</a>
+    <a href="https://somoyerkhbor.com/surga33" target="_blank" rel="dofollow">surga33</a>
+    <a href="https://somoyerkhbor.com/surga22" target="_blank" rel="dofollow">surga22</a>
+    <a href="https://somoyerkhbor.com/surga77" target="_blank" rel="dofollow">surga77</a>
+    <a href="https://somoyerkhbor.com/togelup" target="_blank" rel="dofollow">togelup</a>
+    <a href="https://somoyerkhbor.com/inatogel" target="_blank" rel="dofollow">inatogel</a>
+    <a href="https://somoyerkhbor.com/togelon" target="_blank" rel="dofollow">togelon</a>
+    <a href="https://somoyerkhbor.com/gengtoto" target="_blank" rel="dofollow">gengtoto</a>
+    <a href="https://somoyerkhbor.com/dewatogel" target="_blank" rel="dofollow">dewatogel</a>
+    <a href="https://somoyerkhbor.com/pos4d" target="_blank" rel="dofollow">pos4d</a>
+    <a href="https://somoyerkhbor.com/hometogel" target="_blank" rel="dofollow">hometogel</a>
+    <a href="https://somoyerkhbor.com/lunatogel" target="_blank" rel="dofollow">lunatogel</a>
 </div>
