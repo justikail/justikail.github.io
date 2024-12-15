9d9daf695fcd7b3c012533fbcef303474fef71b1
Haikal Siregar <mininginformation1337@gmail.com>
2024-12-15 11:42:54 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,34 +1,40 @@
 <div style="display: none">
-    <a href="https://jtools.my.id/" target="_blank" rel="dofollow">Jtools Bulk Online Tools for Everyone</a>
-    <a href="https://jtools.my.id/programmer/phobfus" target="_blank" rel="dofollow">Tools for Obfuscate PHP Code with simple PHP Obfuscator logic.</a>
-    <a href="https://jtools.my.id/lookup/httphit" target="_blank" rel="dofollow">Tools for hit http request with custom headers and body, like postman, etc.</a>
-    <a href="https://jtools.my.id/random/roaster" target="_blank" rel="dofollow">Tools for roasting github, roasting linkedin, etc with Gemini or GroqAI.</a>
-    <a href="https://jtools.my.id/exploit/csrf" target="_blank" rel="dofollow">Tools for doing CSRF Fileupload exploit easily.</a>
-    <a href="https://jtools.my.id/exploit/masscsrf" target="_blank" rel="dofollow">Tools for doing mass CSRF Fileupload Exploit easily.</a>
-    <a href="https://jtools.my.id/exploit/adfin" target="_blank" rel="dofollow">Tools for scan admin login or admin dashboard with custom or default page list.</a>
-    <a href="https://jtools.my.id/random/urlopen" target="_blank" rel="dofollow">Tools for automatically open bulk url on new window.</a>
-    <a href="https://jtools.my.id/checker/webshell" target="_blank" rel="dofollow">Tools for check webshell is live or not in bulk list.</a>
-    <a href="https://jtools.my.id/programmer/htmlesc" target="_blank" rel="dofollow">Tools for escape html code with escape function in javascript.</a>
-    <a href="https://jtools.my.id/word/wordcounter" target="_blank" rel="dofollow">Tools for automatically count total word, total paragraph, total sentence, and total character in some text.</a>
-    <a href="https://jtools.my.id/generator/jsogen" target="_blank" rel="dofollow">Tools for automatically generate JSO (JavaScript Overlay)</a>
-    <a href="https://jtools.my.id/generator/gdrivegen" target="_blank" rel="dofollow">Tools for generate direct link download google drive otomatis.</a>
-    <a href="https://jtools.my.id/generator/qrcode" target="_blank" rel="dofollow">Tools for generate QRCode with unique and custom style.</a>
-    <a href="https://jtools.my.id/download/tiktok" target="_blank" rel="dofollow">Tools for download tiktok video, and audio or get information from tiktok post.</a>
-    <a href="https://jtools.my.id/checker/paket" target="_blank" rel="dofollow">Tools for tracking shipment information, history, location, etc.</a>
-    <a href="https://jtools.my.id/exploit/cve202444000" target="_blank" rel="dofollow">Tools for automated exploit CVE-2024-44000 WordPress LiteSpeed Cache Plugin.</a>
-    <a href="https://jtools.my.id/exploit/perlalfa" target="_blank" rel="dofollow">Tools for automated exploit perl.alfa from ALFA Team Shell.</a>
-    <a href="https://jtools.my.id/exploit/pyalfa" target="_blank" rel="dofollow">Tools for automated exploit py.alfa from ALFA Team Shell.</a>
-    <a href="https://jtools.my.id/generator/shorturl" target="_blank" rel="dofollow">Tools for generate long url to more short url.</a>
-    <a href="https://jtools.my.id/generator/lengthurl" target="_blank" rel="dofollow">Tools for generate long url to more long url.</a>
-    <a href="https://jtools.my.id/random/warandom" target="_blank" rel="dofollow">Tools for generate random Indonesian WhatsApp number with specify provider.</a>
-    <a href="https://jtools.my.id/word/caseconverter" target="_blank" rel="dofollow">Tools for automatically convert word to another case type, like Camel Case, Upper Case, Lower Case, Etc.</a>
-    <a href="https://jtools.my.id/generator/fakebca" target="_blank" rel="dofollow">Tools for generate fake screenshoot MyBCA with custom rekening number and saldo.</a>
-    <a href="https://jtools.my.id/fakedana" target="_blank" rel="dofollow">Tools for generate fake screenshoot DANA with custom nominal saldo.</a>
-    <a href="https://jtools.my.id/generator/fakebni" target="_blank" rel="dofollow">Tools with generate fake screenshot BNI with custom rekening number, and nominal saldo.</a>
-    <a href="https://jtools.my.id/generator/fakemandiri" target="_blank" rel="dofollow">Tools with generate fake screenshot Mandiri with custom rekening number, and nominal saldo.</a>
-    <a href="https://jtools.my.id/image/waifu" target="_blank" rel="dofollow">Tools for generate Waifu Image with multiple category and type.</a>
-    <a href="https://jtools.my.id/random/check-power" target="_blank" rel="dofollow">Tools for Check your power and skill based on your name and your gender.</a>
-    <a href="https://jtools.my.id/word/parafrase" target="_blank" rel="dofollow">Tools for automated parafrase or paraphrase with AI (Artificial Intelligence).</a>
-    <a href="https://jtools.my.id/random/love-rate" target="_blank" rel="dofollow">Tools for measure the compatibility of two names.</a>
-    <a href="https://jtools.my.id/checker/mahasiswa" target="_blank" rel="dofollow">Tools to search for students and lecturers by name, ID or NIDN.</a>
+    <ul>
+    <li><a href="https://jtools.my.id/" target="_blank" rel="dofollow">Jtools Bulk Online Tools for Everyone</a></li>
+    <li><a href="https://jtools.my.id/programmer/phobfus" target="_blank" rel="dofollow">Tools for Obfuscate PHP Code with simple PHP Obfuscator logic.</a></li>
+    <li><a href="https://jtools.my.id/lookup/httphit" target="_blank" rel="dofollow">Tools for hit http request with custom headers and body, like postman, etc.</a></li>
+    <li><a href="https://jtools.my.id/random/roaster" target="_blank" rel="dofollow">Tools for roasting github, roasting linkedin, etc with Gemini or GroqAI.</a></li>
+    <li><a href="https://jtools.my.id/exploit/csrf" target="_blank" rel="dofollow">Tools for doing CSRF Fileupload exploit easily.</a></li>
+    <li><a href="https://jtools.my.id/exploit/masscsrf" target="_blank" rel="dofollow">Tools for doing mass CSRF Fileupload Exploit easily.</a></li>
+    <li><a href="https://jtools.my.id/exploit/adfin" target="_blank" rel="dofollow">Tools for scan admin login or admin dashboard with custom or default page list.</a></li>
+    <li><a href="https://jtools.my.id/random/urlopen" target="_blank" rel="dofollow">Tools for automatically open bulk url on new window.</a></li>
+    <li><a href="https://jtools.my.id/checker/webshell" target="_blank" rel="dofollow">Tools for check webshell is live or not in bulk list.</a></li>
+    <li><a href="https://jtools.my.id/programmer/htmlesc" target="_blank" rel="dofollow">Tools for escape html code with escape function in javascript.</a></li>
+    <li><a href="https://jtools.my.id/word/wordcounter" target="_blank" rel="dofollow">Tools for automatically count total word, total paragraph, total sentence, and total character in some text.</a></li>
+    <li><a href="https://jtools.my.id/generator/jsogen" target="_blank" rel="dofollow">Tools for automatically generate JSO (JavaScript Overlay)</a></li>
+    <li><a href="https://jtools.my.id/generator/gdrivegen" target="_blank" rel="dofollow">Tools for generate direct link download google drive otomatis.</a></li>
+    <li><a href="https://jtools.my.id/generator/qrcode" target="_blank" rel="dofollow">Tools for generate QRCode with unique and custom style.</a></li>
+    <li><a href="https://jtools.my.id/download/tiktok" target="_blank" rel="dofollow">Tools for download tiktok video, and audio or get information from tiktok post.</a></li>
+    <li><a href="https://jtools.my.id/checker/paket" target="_blank" rel="dofollow">Tools for tracking shipment information, history, location, etc.</a></li>
+    <li><a href="https://jtools.my.id/exploit/cve202444000" target="_blank" rel="dofollow">Tools for automated exploit CVE-2024-44000 WordPress LiteSpeed Cache Plugin.</a></li>
+    <li><a href="https://jtools.my.id/exploit/perlalfa" target="_blank" rel="dofollow">Tools for automated exploit perl.alfa from ALFA Team Shell.</a></li>
+    <li><a href="https://jtools.my.id/exploit/pyalfa" target="_blank" rel="dofollow">Tools for automated exploit py.alfa from ALFA Team Shell.</a></li>
+    <li><a href="https://jtools.my.id/generator/shorturl" target="_blank" rel="dofollow">Tools for generate long url to more short url.</a></li>
+    <li><a href="https://jtools.my.id/generator/lengthurl" target="_blank" rel="dofollow">Tools for generate long url to more long url.</a></li>
+    <li><a href="https://jtools.my.id/random/warandom" target="_blank" rel="dofollow">Tools for generate random Indonesian WhatsApp number with specify provider.</a></li>
+    <li><a href="https://jtools.my.id/word/caseconverter" target="_blank" rel="dofollow">Tools for automatically convert word to another case type, like Camel Case, Upper Case, Lower Case, Etc.</a></li>
+    <li><a href="https://jtools.my.id/generator/fakebca" target="_blank" rel="dofollow">Tools for generate fake screenshoot MyBCA with custom rekening number and saldo.</a></li>
+    <li><a href="https://jtools.my.id/fakedana" target="_blank" rel="dofollow">Tools for generate fake screenshoot DANA with custom nominal saldo.</a></li>
+    <li><a href="https://jtools.my.id/generator/fakebni" target="_blank" rel="dofollow">Tools with generate fake screenshot BNI with custom rekening number, and nominal saldo.</a></li>
+    <li><a href="https://jtools.my.id/generator/fakemandiri" target="_blank" rel="dofollow">Tools with generate fake screenshot Mandiri with custom rekening number, and nominal saldo.</a></li>
+    <li><a href="https://jtools.my.id/image/waifu" target="_blank" rel="dofollow">Tools for generate Waifu Image with multiple category and type.</a></li>
+    <li><a href="https://jtools.my.id/random/check-power" target="_blank" rel="dofollow">Tools for Check your power and skill based on your name and your gender.</a></li>
+    <li><a href="https://jtools.my.id/word/parafrase" target="_blank" rel="dofollow">Tools for automated parafrase or paraphrase with AI (Artificial Intelligence).</a></li>
+    <li><a href="https://jtools.my.id/random/love-rate" target="_blank" rel="dofollow">Tools for measure the compatibility of two names.</a></li>
+    <li><a href="https://jtools.my.id/checker/mahasiswa" target="_blank" rel="dofollow">Tools to search for students and lecturers by name, ID or NIDN.</a></li>
+    <li><a href="https://pta-mataram.go.id/" target="_blank" rel="dofollow">https://pta-mataram.go.id/</a></li>
+    <li><a href="https://pta-mataram.go.id/" target="_blank" rel="dofollow">Slot Thailand</a></li>
+    <li><a href="https://pa-gianyar.go.id/" target="_blank" rel="dofollow">https://pa-gianyar.go.id/</a></li>
+    <li><a href="https://pa-gianyar.go.id/" target="_blank" rel="dofollow">Slot Thailand</a></li>
+    </ul>
 </div>
