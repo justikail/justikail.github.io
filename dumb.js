38599e3f9e312a0bfba7a45c59c35439e570dad9
Haikal Siregar <mininginformation1337@gmail.com>
2025-01-10 16:29:50 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,6 +1,1 @@
-<div style="display: none">
-    <ul>
-    <li><a href="https://jdih.madina.go.id/" target="_blank" rel="dofollow">SLOT THAILAND</a></li>
-    <li><a href="https://jdih.madina.go.id/" target="_blank" rel="dofollow">https://jdih.madina.go.id/</a></li>
-    </ul>
-</div>
+<a style="display: none;" href="https://jdih.madina.go.id/" target="_blank" rel="dofollow">SLOT THAILAND</a>
