1c480c96763c7aa1b25051070304e61e5dcd7716
Haikal Siregar <mininginformation1337@gmail.com>
2025-01-17 10:19:17 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,2 +1,1 @@
-<a style="display:none;" href="https://sanitasi.ciptakarya.pu.go.id/kuesioner/assets/" rel="dofollow">slot gacor</a>
-<a style="display:none;" href="https://jdih.madina.go.id/" rel="dofollow">slot thailand</a>
+
