1f966645946ef1c4caed90c49347f20e39fd0c54
Haikal Siregar <mininginformation1337@gmail.com>
2025-03-01 16:54:14 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,2 +1,4 @@
 <div style="display: none;">
+  <a href="https://satanicteam.pro/" target="_blank" rel="dofollow">SatanicTeam</a>
+  <a href="https://satanicteam.pro/" target="_blank" rel="dofollow">https://satanicteam.pro/</a>
 </div>
