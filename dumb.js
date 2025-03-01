81e53d02bf3aa15bcbedf60045e65418c5e61da8
Haikal Siregar <mininginformation1337@gmail.com>
2025-03-01 16:49:55 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,4 +1,2 @@
 <div style="display: none;">
-  <a href="https://jdih.bphmigas.go.id/" target="_blank" rel="dofollow">Slot Thailand</a>
-  <a href="https://jdih.bphmigas.go.id/" target="_blank" rel="dofollow">https://jdih.bphmigas.go.id/</a>
 </div>
