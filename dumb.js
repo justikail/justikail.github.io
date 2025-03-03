b3300e1c6be2430ead6dfa672e2e83fcf5e392f2
Haikal Siregar <mininginformation1337@gmail.com>
2025-03-03 22:44:46 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,4 +1,6 @@
 <div style="display: none;">
   <a href="https://satanicteam.pro/" target="_blank" rel="dofollow">SatanicTeam</a>
   <a href="https://satanicteam.pro/" target="_blank" rel="dofollow">https://satanicteam.pro/</a>
+  <a href="https://jdih.bphmigas.go.id/" target="_blank" rel="dofollow">Slot Thailand</a>
+  <a href="https://jdih.bphmigas.go.id/" target="_blank" rel="dofollow">https://jdih.bphmigas.go.id/</a>
 </div>
