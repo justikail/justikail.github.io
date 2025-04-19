436c4a0e98fac939ffe6deca632d9ce05ab197b2
Haikal Siregar <mininginformation1337@gmail.com>
2025-04-20 00:47:26 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,4 +1,4 @@
 <div style="display: none;">
   <a href="https://stipjakarta.ac.id/storage/" target="_blank" rel="dofollow">Slot Thailand</a>
-  <a href="https://stipjakarta.ac.id/storage/" target="_blank" rel="dofollow">https://stipjakarta.ac.id/storage/</a>
+  <a href="https://siantarcorner.com/" target="_blank" rel="dofollow">Ws168</a>
 </div>
