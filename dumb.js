34d28852b7b9532b449976af0115b3b79fc146ab
Haikal Siregar <mininginformation1337@gmail.com>
2025-05-21 22:56:28 +0700

    Update dumb.js

--- dumb.js
+++ dumb.js
@@ -1,4 +1,4 @@
 <div style="display: none;">
   <a href="https://povertyconqueror.com/" target="_blank" rel="dofollow">Slot Gacor</a>
-  <a href="https://www.icm.org.pk/" target="_blank" rel="dofollow">SV388</a>
+  <a href="https://sman1adonaratimur.sch.id/" target="_blank" rel="dofollow">https://sman1adonaratimur.sch.id/</a>
 </div>
