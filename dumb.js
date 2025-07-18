f130b4884b011dc039117959271136abc161a29c
Haikal Siregar <mininginformation1337@gmail.com>
2025-07-18 13:04:27 +0700

    Delete dumb.js

--- dumb.js
+++
@@ -1,4 +0,0 @@
-<div style="display: none;">
-  <a href="https://povertyconqueror.com/" target="_blank" rel="dofollow">Slot Gacor</a>
-  <a href="https://sman1adonaratimur.sch.id/" target="_blank" rel="dofollow">https://sman1adonaratimur.sch.id/</a>
-</div>
